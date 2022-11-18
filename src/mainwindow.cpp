e5489b7f21599f80746c9d960e9a7a31c6874729
xusheng <3380590+jeffli678@users.noreply.github.com>
2022-11-18 23:43:02 +0800

    Basic main window code

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,8 +1,17 @@
 #include "mainwindow.h"
+#include <QMenuBar>
+#include <QMenu>
 
 MainWindow::MainWindow()
 {
+    auto menuBar = new QMenuBar(this);
+    auto fileMenu = new QMenu("File");
 
+    auto helpMenu = new QMenu("Help");
+
+    menuBar->addMenu(fileMenu);
+    menuBar->addMenu(helpMenu);
+    setMenuBar(menuBar);
 }
 
 
