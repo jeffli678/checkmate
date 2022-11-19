da669bb7a4faad1cedbcde35a29b178c4ec23ba8
xusheng <>
2022-11-19 12:08:21 +0800

    Move mainwindow code

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,13 +4,20 @@
 
 MainWindow::MainWindow()
 {
+    setWindowTitle("Checkmate");
+
     auto menuBar = new QMenuBar(this);
     auto fileMenu = new QMenu("File");
+    auto newAction = new QAction("New...");
+    fileMenu->addAction(newAction);
 
     auto helpMenu = new QMenu("Help");
+    auto aboutAction = new QAction("About");
+    helpMenu->addAction(aboutAction);
 
     menuBar->addMenu(fileMenu);
     menuBar->addMenu(helpMenu);
+
     setMenuBar(menuBar);
 }
 
